3d882abf7837c5bc92b57958bff43e0d312cd2ec
Einars <einars431@gmail.com>
2020-10-31 18:12:59 +0200

    jauninajums

---
+++ funkcija4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float skaitlis1;
+float skaitlis2;
+char simbols;
+
+int funkcija(float skaitlis1, float skaitlis2, char simbols){
+	float rezultats;
+	if(simbols = '+'){
+		rezultats = skaitlis1 + skaitlis2;
+	}else if(simbols = '-'){
+		rezultats = skaitlis1 - skaitlis2;
+	}else if(simbols = '*'){
+		rezultats = skaitlis1 * skaitlis2;
+	}else if(simbols = '/'){
+		rezultats = skaitlis1 / skaitlis2;
+	}else{
+		printf("Kļūda. Nederīgs operātors.\n");
+	}
+	return rezultats;
+}
+
+int main(){
+	printf("Lūdzu ievadiet 1.skaitli: \n");
+	scanf("%f", &skaitlis1);
+	printf("Lūdzu ievadiet 2.skaitli: \n");
+	scanf("%f", &skaitlis2);
+	printf("Kādu operāciju jūs izvēlaties?\n");
+	scanf("%c", &simbols);
+	float result = funkcija(skaitlis1, skaitlis2, simbols);
+	printf("Rezultāts ir %f\n", result);
+}
